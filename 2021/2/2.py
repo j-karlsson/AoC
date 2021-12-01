c5d9627f2db896c55805fb24379c697be3df133d
j-karlsson <53605491+j-karlsson@users.noreply.github.com>
2021-12-01 09:20:01 +0100

    Prepare day 2

---
+++ 2021/2/2.py
@@ -0,0 +1,22 @@
+"""Advent of code 2021 - Day 2."""
+import os
+import sys
+
+INPUT = os.path.join(sys.path[0], 'input.txt')
+
+with open(INPUT) as file:
+    input_values = [int(line.rstrip()) for line in file]
+
+
+def part1(values):
+    """Part1."""
+    pass
+
+
+def part2(values):
+    """Part2."""
+    pass
+
+
+print(f"part one: {part1(input_values)}")
+print(f"part two: {part2(input_values)}")
