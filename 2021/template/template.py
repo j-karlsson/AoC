bb218125a9a170e6bf24274f9cedd97a63aaac11
j-karlsson <53605491+j-karlsson@users.noreply.github.com>
2021-12-07 16:39:28 +0100

    Day 5

---
+++ 2021/template/template.py
@@ -0,0 +1,22 @@
+"""Advent of code 2021 - Day 5."""
+import os
+import sys
+
+INPUT = os.path.join(sys.path[0], 'input_test.txt')
+
+
+with open(INPUT) as file:
+
+
+def part1(values):
+    """Part1."""
+    pass
+
+
+def part2(values):
+    """Part2."""
+    pass
+
+
+# print(f"part one: {part1(input_values)}")
+# print(f"part two: {part2(input_values)}")
