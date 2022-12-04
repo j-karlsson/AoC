d4749959ca3c5a5830d3da16093bcbf145a1b3b0
endor <1937941+endor-force@users.noreply.github.com>
2022-12-04 10:01:26 +0100

    Day 4 - Final

--- 2022-js/4/day-4.js
+++ 2022-js/4/day-4.js
@@ -28,29 +28,27 @@ function getRange(from_val, to_val) {
 function part1(input) {
 	let score = 0
 	for (let row of input) {
-
 		let p1full = getRange(row[0][0], row[0][1])
 		let p2full = getRange(row[1][0], row[1][1])
 
 		// Check if the full left range exist in the right, or the full right in the left range.
 		p1full.filter(number => p2full.includes(number)).length == p1full.length ||
 		p2full.filter(number => p1full.includes(number)).length == p2full.length ? score ++ : false
-
 	}
+
 	return score
 }
 
 function part2(input) {
 	let score = 0
 	for (let row of input) {
-
 		let p1full = getRange(row[0][0], row[0][1])
 		let p2full = getRange(row[1][0], row[1][1])
 
 		// Check if any of the numbers in the left range overlap with right range.
 		p1full.filter(number => p2full.includes(number)).length ? score ++ : false
-
 	}
+
 	return score
 }
 
