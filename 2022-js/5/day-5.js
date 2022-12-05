6b9b5e79a338c781ec9076daa98f320b437f9a48
j-karlsson <jonas.karlsson@inter.ikea.com>
2022-12-05 16:52:29 +0100

    Day 5 - Added column counter + comments

--- 2022-js/5/day-5.js
+++ 2022-js/5/day-5.js
@@ -4,72 +4,77 @@ const { readFileSync } = require("fs")
 const YEAR = 2022
 const DAY = 5
 const FILE = "input"
-const TABLECOLS = 9
-
 
 // Read input data and split by line breaks, commas and then by dash. Format as number.
 function syncReadFile(filename) {
-	let contents = readFileSync(filename, "utf-8")
-		.split(/\r?\n\r?\n/)
+	let contents = readFileSync(filename, "utf-8").split(/\r?\n\r?\n/)
 
 	return contents
 }
 
 function getTabledata(tableRows) {
-
 	let tableInput = tableRows.split(/\r?\n/).reverse()
+	let numberOfColumns = parseInt(tableInput[0].trim().split(" ").slice(-1))
+
+	// Find character positions based on position of numbers.
 	let positions = []
-	
-	for(let i=1; i<= TABLECOLS; i++){
+	for (let i = 1; i <= numberOfColumns; i++) {
 		positions.push(tableInput[0].indexOf(i))
 	}
 
+	// Remove numbers.
 	tableInput.shift()
+
+	//Loop through each postion and gather letters in that stack.
 	let tableOutput = []
-	for(let position of positions) {
+	for (let position of positions) {
 		let stack = []
-		for(let row of tableInput){
-			if(row.charAt(position) != ' '){
-			stack.push(row.charAt(position))
+		for (let row of tableInput) {
+			if (row.charAt(position) != " ") {
+				stack.push(row.charAt(position))
 			}
 		}
 		tableOutput.push(stack)
 	}
 
 	return tableOutput
-	
 }
 
-function getMoves (rawMoves) {
-	let moves = rawMoves.split(/\r?\n/)
-						 .map(v1 => v1.split(" "))
+function getMoves(rawMoves) {
+	let moves = rawMoves.split(/\r?\n/).map((v1) => v1.split(" "))
 	let movesOut = []
-	for(let move of moves) {
-		let moveRow = []
-		for(let position of move) {
-			if(!isNaN(position)) moveRow.push(position)
 
+	// Get the numbers from string.
+	for (let move of moves) {
+		let moveRow = []
+		for (let position of move) {
+			if (!isNaN(position)) moveRow.push(position)
 		}
 		movesOut.push(moveRow)
 	}
-	return(movesOut)
+	return movesOut
 }
 
 function part1(input) {
 	let boxes = getTabledata(input[0])
 	let moves = getMoves(input[1])
+	let numberOfBoxes = move[0]
 
-	for(let move of moves) {
-		for(let i=0;i < move[0]; i++){
-			boxes[move[2]-1].push(boxes[move[1]-1].pop())
+	// Start moving boxes one by one.
+	for (let move of moves) {
+		let fromStack = move[1] - 1
+		let toStack = move[2] - 1
+
+		for (let i = 0; i < numberOfBoxes; i++) {
+			boxes[fromStack].push(boxes[toStack].pop())
 		}
 	}
 
+	// Get the letter of the box in the top of each stack.
 	let result = []
-	for(box of boxes) {
+	for (box of boxes) {
 		result.push(box.pop())
 	}
-	
 
 	return result.join("")
 }
@@ -77,19 +82,27 @@ function part1(input) {
 function part2(input) {
 	let boxes = getTabledata(input[0])
 	let moves = getMoves(input[1])
-	
-	for(let move of moves) {
-		boxes[move[2]-1] = boxes[move[2]-1].concat(boxes[move[1]-1].splice(-move[0]))
+	let numberOfBoxes = move[0]
+
+	// Start moving boxes many at once.
+	for (let move of moves) {
+		let fromStack = move[1] - 1
+		let toStack = move[2] - 1
+
+		// Concatenate existing array with removed elements.
+		boxes[toStack] = boxes[toStack].concat(
+			boxes[fromStack].splice(-numberOfBoxes)
+		)
 	}
 
+	// Get the letter of the box in the top of each stack.
 	let result = []
-	for(box of boxes) {
+	for (box of boxes) {
 		result.push(box.pop())
 	}
-	
+
 	return result.join("")
 }
-
 
 function main(year, day, file) {
 	const input = syncReadFile(`./${year}-js/${day}/${file}.txt`)
