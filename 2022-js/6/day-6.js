0c82593676c7b9d05e95b1477e5550a50345a362
endor <1937941+endor-force@users.noreply.github.com>
2022-12-06 13:02:28 +0100

    Day 6 minor clean

--- 2022-js/6/day-6.js
+++ 2022-js/6/day-6.js
@@ -6,45 +6,39 @@ const FILE = "input"
 
 // Read input data and split by line breaks, commas and then by dash. Format as number.
 function syncReadFile(filename) {
-  let contents = readFileSync(filename, "utf-8").split("")
+	let contents = readFileSync(filename, "utf-8").split("")
 
-  return contents
+	return contents
 }
 
-function part(input, chars) {
-  let result = 0
+function part(input, targetLength) {
+	let result = 0
 
-  for (index = 0;index < input.length; index++) {
-    let uniqueChars = new Set()
+	for (position = targetLength-1 ; position < input.length; position++) {
+		let uniqueChars = new Set()
 
-	// Start running from the expected starting point given in chars (e.g. 4 or 14).
-	// Loop the number of characters back in the array and add them to the set.
-    if (index > chars - 1) {
-		for (i = 0; i < chars; i++) {
-        	uniqueChars.add(input[index - i])
-    	}
-    }
+		// Loop the number of characters back in the array and add them to the set.
+		for (i = 0; i < targetLength; i++) {
+			uniqueChars.add(input[position - i])
+		}
 
-	// If the length of the set (unique values) match the given length of chars, we are done.
-    if (uniqueChars.size == chars && result == 0) {
-      result = index + 1
-      break
-    }
-  }
-
-  return result
+		// If the length of the set (unique values) match the target length of chars, we are done.
+		if (uniqueChars.size == targetLength && result == 0) {
+			return position + 1
+		}
+	}
 }
 
 function main(year, day, file) {
-  const input = syncReadFile(`./${year}-js/${day}/${file}.txt`)
+	const input = syncReadFile(`./${year}-js/${day}/${file}.txt`)
 
-  console.log(`Results part 1: ${part(input, 4)}`)
-  // 7
-  // 1175
+	console.log(`Results part 1: ${part(input, 4)}`)
+	// 7
+	// 1175
 
-  console.log(`Results part 2: ${part(input, 14)}`)
-  // 19
-  // 3217
+	console.log(`Results part 2: ${part(input, 14)}`)
+	// 19
+	// 3217
 }
 
 main(YEAR, DAY, FILE)
